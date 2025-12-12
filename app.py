6b72346af94b988f76acf8fcc3e00139a52b1723
nguyenbinhym-lab <nguyenbinhym@gmail.com>
2025-12-12 15:59:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -190,8 +190,8 @@ st.markdown("""
 # --- 4. GIAO DIỆN CHÍNH ---
 st.markdown("""
 <div class="main-header">
-    <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG lớp 5 (NLS)</h1>
-    <p>Tác giả: Sĩ Thị Sính - Trường PTDTBT Tiểu học Mậu Long - ĐT: 0989151362 </p>
+    <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
+    <p>Tác giả: Phạm Đức Huy - Trường PTDTBT Tiểu học Mậu Long - ĐT: 0814430886 </p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Sĩ Thị Sính - Trường PTDTBT Tiểu học Mậu Long - ĐT: 0989151362</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Phạm Đức Huy - Trường PTDTBT Tiểu học Mậu Long - ĐT: 0814430886</div>", unsafe_allow_html=True)
